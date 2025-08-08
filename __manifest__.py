849f12e6ba678c841bcbd7b1ffc1b4eb5ae1ea1a
charly <kouasseu.tamayeu@gmail.com>
2025-08-08 21:17:41 +0200

    initialisation et creation du model sans champ

---
+++ __manifest__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'estate',
+    'version': '0.0.1',
+    'depends': ['base'],
+}
---
+++ models/estate_property.py
@@ -0,0 +1,5 @@
+from odoo import models
+
+class estatePropertyModel(models.Model):
+    _name = "estate.estate_property"
+    _description = """ Un model pour les propriété de mes imobiliers."""
