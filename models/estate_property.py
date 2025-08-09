843b6b4858d3c98df50350cac62007551440689f
charly <kouasseu.tamayeu@gmail.com>
2025-08-09 18:41:07 +0200

    ajout des champs dans le model estate_property

--- models/estate_property.py
+++ models/estate_property.py
@@ -1,5 +1,6 @@
-from odoo import models
+from odoo import models, fields
 
 class estatePropertyModel(models.Model):
     _name = "estate.estate_property"
     _description = """ Un model pour les propriété de mes imobiliers."""
+    name = fields.Char()
